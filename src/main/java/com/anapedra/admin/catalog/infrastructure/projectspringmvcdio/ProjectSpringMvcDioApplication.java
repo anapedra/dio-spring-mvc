10235eef11616d4eb608e7adf5dbe9f907105eb4
anapedra <anapedra.mil@gmail.com>
2024-02-01 03:14:11 -0300

    Project spring-mvc-dio created

---
+++ src/main/java/com/anapedra/admin/catalog/infrastructure/projectspringmvcdio/ProjectSpringMvcDioApplication.java
@@ -0,0 +1,13 @@
+package com.anapedra.admin.catalog.infrastructure.projectspringmvcdio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringMvcDioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSpringMvcDioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anapedra/admin/catalog/infrastructure/projectspringmvcdio/ProjectSpringMvcDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anapedra.admin.catalog.infrastructure.projectspringmvcdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringMvcDioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
